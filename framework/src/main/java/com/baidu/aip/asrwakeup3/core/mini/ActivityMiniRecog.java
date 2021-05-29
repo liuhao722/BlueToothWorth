fe8757bc69781f1eb0859d032945a2e27a18f888
刘浩 <liuhao01@ienglish.com>
2021-05-29 11:21:19 +0800

    init

--- framework/src/main/java/com/baidu/aip/asrwakeup3/core/mini/ActivityMiniRecog.java
+++ framework/src/main/java/com/baidu/aip/asrwakeup3/core/mini/ActivityMiniRecog.java
@@ -18,7 +18,7 @@ import com.baidu.speech.EventListener;
 import com.baidu.speech.EventManager;
 import com.baidu.speech.EventManagerFactory;
 import com.baidu.speech.asr.SpeechConstant;
-import com.worth.framework.R;
+import com.worth.bluetooth.R;
 
 import org.json.JSONObject;
 
--- framework/src/main/java/com/baidu/tts/sample/BaseActivity.java
+++ framework/src/main/java/com/baidu/tts/sample/BaseActivity.java
@@ -19,6 +19,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.worth.bluetooth.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
